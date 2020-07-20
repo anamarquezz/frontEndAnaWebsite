d2a1e5eaba3e549be8d13c44932597de6bbc2afe
AnaMarquez <anamarquez.edu@hotmail.com>
2020-07-19 23:32:23 -0700

    first page

---
+++ annaweb/src/views/Home.vue
@@ -0,0 +1,46 @@
+
+<template>
+  <div class="principal">
+    <div class="div-izq">
+      <div class="imgana">
+      </div>
+      <div class="links">
+          <ul class="social-icons-list">
+                <li><a href="https://github.com/anamarquezz" class="social-link" target="_blank"><i class="fab fa-facebook-f"></i></a></li>
+                <li><a href="https://www.linkedin.com/in/ana-marquez/" class="social-link" target="_blank"><i class="fab fa-linkedin"></i></a></li>
+          </ul>
+      </div>
+    </div>
+    <div class="div-der">
+      <div class="black-block">
+        <div>
+          <p class="nombre">Ana Márquez</p>
+          <p class="profession">Full Stack Developer</p>
+        </div>
+        <div class="img-viewmore">
+          <div class="img-izq">
+              <img class="img-illus" src="@/assets/img/img3.webp" alt="">
+          </div>
+          <div class="imf-der">
+              <link rel="stylesheet" href="">
+              <p>View More</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  components: {
+  }
+}
+</script>
+<style>
+    @import url('../assets/css/inicio.css');
+</style>
