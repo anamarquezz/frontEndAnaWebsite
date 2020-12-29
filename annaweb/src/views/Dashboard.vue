eb51ef1c93c1e9e4a2c77213bc4814c27c395b5f
AnaMarquez <anamarquez.edu@hotmail.com>
2020-12-28 23:17:01 -0800

    updates

---
+++ annaweb/src/views/Dashboard.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="dashboard">
+        <div class="web">
+            <div class="menu-izq"></div>
+            <div class="nav-bar">
+                <div>Projects</div>
+                <div>
+                    <ul
+                     class="search-icons">
+                         <li><a href="https://github.com/anamarquezz" target="_blank"><i class="fab fa-github-square"></i></a></li>
+                        <li><a href="https://github.com/anamarquezz" target="_blank"><i class="fab fa-github-square"></i></a></li>
+                        <li><a href="https://www.linkedin.com/in/ana-marquez/" target="_blank"><i class="fab fa-linkedin"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="projects-cards"></div>
+        </div>
+        <div class="mobil">
+            <div class="menu--mobil-down">
+            </div>
+            <div class="main-mobil">
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+// @ is an alias to /src
+export default {
+  name: 'Dashboard',
+  components: {}
+}
+</script>
+<style>
+  @import url('../assets/css/dashboard.css');
+
+</style>
